b65df04798339f9b3957cccfc6f24db023499039
David <david@heik@web.de>
2017-03-13 13:52:51 +0100

    [add] hinzu der checkbox.php

---
+++ private/php/view/templates/partials/form/checkbox.php
@@ -0,0 +1,22 @@
+<?php 
+    $name = $name ?? 'checkbox';
+    $id = $id ?? $name;
+    $required = $required ?? false;    
+    $minlength = $minlength ?? 0;
+    $maxlength = $maxlength ?? 0;
+    $mask = $mask?? false;
+    $placeholder = $placeholder ?? '';
+    $label = $label ?? 'label';
+    $remote = $remote ?? '';
+    $remoteMessage = $remoteMessage ?? '';
+?>
+<div class="form-group">
+    <label class="control-label" for="<?=$this->e($id)?>"><?= $label ?></label>
+    <input
+        class="form-control"
+        id="<?=$this->e($id)?>"
+        name="<?=$this->e($name)?>"
+        type= "checkbox">
+        <?php if ($required): ?> required <?php endif; ?>
+    />
+</div>
