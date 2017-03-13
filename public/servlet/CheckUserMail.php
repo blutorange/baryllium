09221e6a152a45fd8cf1b68911af71cc5f20471f
David <david@heik@web.de>
2017-03-13 13:05:12 +0100

    [Zwischenstand] CheckUsermail hinzu, überprüft ob eien E-mail bereits in der Datenbank vorhanden ist.

---
+++ public/servlet/CheckUserMail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Servlet;
+
+require_once '../../private/bootstrap.php';
+
+use \Dao\UserDao;
+
+class CheckUserMail extends AbstractRestServlet {
+    public function rest(array & $requestData, array & $responseData) : int {
+        $code = 200;
+        $mail = $requestData['mail'];
+        $exists = false;
+        if (isset($mail)) {
+            $exists = (new UserDao($this->getEm()))->findOneByField('mail', $mail) != null;
+            $code = $exists ? 412 : 200;
+        }
+        else {
+            $this->setError($responseData, 'Illegal request', 'No email given.');
+            $code = 400;
+        }
+        $responseData['exists'] = $exists;
+        $responseData['mail'] = $mail;
+        return $code;
+    }
+}
+
+(new CheckUsername())->process();
