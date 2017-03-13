46a74f877a6775304692849f60370ec5bf0ab8bb
David <david@heik@web.de>
2017-03-13 12:30:33 +0100

    [Zwischenstand] DB Felder hinzu  + setter / getter

--- private/php/entity/User.php
+++ private/php/entity/User.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Entity;
 
 use Gettext\Translator;
@@ -19,22 +20,44 @@ use Dao\UserDao;
  * @author madgaksha
  */
 class User extends AbstractEntity {
+
     const TABLE_NAME = "user";
-    
+
+    /**
+     * @Column(type="string", length=64, unique=true, nullable=false)
+     * @var string
+     * First name of this user.
+     */
+    protected $firstname;
+
+    /**
+     * @Column(type="string", length=64, unique=true, nullable=false)
+     * @var string
+     * Last name of this user.
+     */
+    protected $lastname;
+
     /**
      * @Column(type="string", length=64, unique=true, nullable=false)
      * @var string
      * User name of this user.
      */
     protected $username;
-       
+
+    /**
+     * @Column(type="string", length=255, unique=true, nullable=false)
+     * @var string
+     * E-Mail of this user.
+     */
+    protected $mail;
+
     /**
      * @Column(type="string", length=255, unique=false, nullable=false)
      * @var string
      * Hashed password of this user.
      */
     protected $pwdhash;
-    
+
     /**
      * @ManyToMany(targetEntity="UserGroup")
      * @JoinTable(name="users_groups",
@@ -44,7 +67,42 @@ class User extends AbstractEntity {
      * @var ArrayCollection All groups this user belongs to.
      */
     protected $groups;
-    
+
+    /**
+     * @Column(type="date", unique=false, nullable=true)
+     * @var string
+     * Date when registered.
+     */
+    protected $regdate;
+
+    /**
+     * @Column(type="date", unique=false, nullable=true)
+     * @var string
+     * Date when activated.
+     */
+    protected $activatedate;
+
+    /**
+     * @Column(type="string", length=255, unique=false, nullable=true)
+     * @var string
+     * Token for activation.
+     */
+    protected $activatetoken;
+
+    /**
+     * @Column(type="binary", unique=false, nullable=true)
+     * @var string
+     * When the user is activated, we change the bool from FALSE 0 to TRUE 1.
+     */
+    protected $isactivated;
+
+    /**
+     * @Column(type="blob", unique=false, nullable=true)
+     * @var bolb
+     * User profilpicture
+     */
+    protected $avatar;
+
     /**
      * @var string NOT persisted. Only saved temporally for validation etc. Nullable.
      */
@@ -54,18 +112,43 @@ class User extends AbstractEntity {
         $this->groups = new \Doctrine\Common\Collections\ArrayCollection();
         $this->sessout = 0;
     }
-    
-    
+
+    public function setFirstname(string $firstname) {
+        $this->firstname = $firstname;
+    }
+
+    public function getFirstname(): string {
+        return $this->firstname;
+    }
+
+    public function setLastname(string $lastname) {
+        $this->lastname = $lastname;
+    }
+
+    public function getLastname(): string {
+        return $this->Lastname;
+    }
+
     public function setUsername(string $username) {
         $this->username = $username;
     }
-    public function getUsername() : string {
+
+    public function getUsername(): string {
         return $this->username;
     }
 
-    public function getPwdHash() : string {
+    public function setMail(string $mail) {
+        $this->mail = $mail;
+    }
+
+    public function getMail(): string {
+        return $this->mail;
+    }
+
+    public function getPwdHash(): string {
         return $this->pwdhash;
     }
+
     public function setPwdHash(string $pwdhash) {
         $this->pwdhash = $pwdhash;
     }
@@ -73,11 +156,12 @@ class User extends AbstractEntity {
     public function getGroups() {
         return $this->groups;
     }
+
     public function setGroups(\Doctrine\Common\Collections\ArrayCollection $groups) {
         $groups->
-        $this->groups = $groups;
+                $this->groups = $groups;
     }
-    
+
     public function addToGroup(UserGroup $group) {
         if ($group != null) {
             if ($this->groups == null) {
@@ -86,7 +170,7 @@ class User extends AbstractEntity {
             $this->groups->add($group);
         }
     }
-    
+
     /**
      * Note that passwords are stored hashed with a salt.
      * @param string $password Password to set.
@@ -97,12 +181,12 @@ class User extends AbstractEntity {
             return;
         $this->setPwdHash(\EncryptionUtil::hashPwd($password));
     }
-    
-    public function verifyPassword(string $password) : bool {
+
+    public function verifyPassword(string $password): bool {
         return \EncryptionUtil::verifyPwd($password, $this->pwdhash);
     }
-    
-    public function validate(array & $errMsg, Translator $translator) : bool {
+
+    public function validate(array & $errMsg, Translator $translator): bool {
         $valid = true;
         if (empty($this->username)) {
             array_push($errMsg, Message::dangerI18n('error.validation', 'error.user.empty', $translator));
@@ -111,38 +195,37 @@ class User extends AbstractEntity {
         if (empty($this->password)) {
             array_push($errMsg, Message::dangerI18n('error.validation', 'error.pass.empty', $translator));
             $valid = false;
-        }
-        else if (\EncryptionUtil::isWeakPwd($this->password)) {
+        } else if (\EncryptionUtil::isWeakPwd($this->password)) {
             array_push($errMsg, Message::dangerI18n('error.security', 'error.pass.weak', $translator));
             $valid = false;
-        }
-        else if (empty($this->pwdhash)) {
+        } else if (empty($this->pwdhash)) {
             $this->setPassword($this->password);
         }
         return $valid;
     }
-    
-    public function validateMore(array & $errMsg, EntityManager $em, Translator $translator) : bool {
+
+    public function validateMore(array & $errMsg, EntityManager $em, Translator $translator): bool {
         $valid = true;
         if ($this->existsUsername($em)) {
             array_push($errMsg, Message::dangerI18n('error.validation', 'register.user.exists', $translator));
-            $valid = false;            
+            $valid = false;
         }
         return $valid;
     }
-    
-    public function existsUsername(EntityManager $em) : bool {
+
+    public function existsUsername(EntityManager $em): bool {
         return (new \Dao\UserDao($em))->findOneByField('username', $this->username) != null;
     }
-    
-    public function getDao(EntityManager $em) : UserDao {
+
+    public function getDao(EntityManager $em): UserDao {
         return new UserDao($em);
     }
-    
-    public static function getAnon() : User {
+
+    public static function getAnon(): User {
         $user = new User();
         $user->setUsername("anon");
         $user->setId(AbstractEntity::$INVALID_ID);
         return $user;
     }
+
 }
