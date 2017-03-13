474daec1ab3ff084642103417586ec2ba702db65
David <david@heik@web.de>
2017-03-13 13:16:41 +0100

    [fix] handled warning when lang param does not exist

--- private/php/PortalSessionHandler.php
+++ private/php/PortalSessionHandler.php
@@ -85,7 +85,7 @@ class PortalSessionHandler extends SessionHandler {
     }
 
     public function getLang() : string {
-        $lang = $_REQUEST["lang"];
+        $lang = array_key_exists('lang', $_REQUEST) ? $_REQUEST['lang'] : '';
         if (empty($lang)) {
             $lang = $_SESSION["lang"];
         }        
