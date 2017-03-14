b2c97241d8e2f882245ff32cf8741fd4dccf4277
David <david@heik@web.de>
2017-03-14 17:42:53 +0100

    [fix] Wenn sich ein Nutzer auf der Seite registiert hat, wird er in der Tabelle User angelegt, und die EMail die an ihn gesendet werden soll, landet in der Tabelle Mail

--- public/controller/register.php
+++ public/controller/register.php
@@ -6,6 +6,7 @@ require_once '../../private/bootstrap.php';
 
 use Controller\AbstractController;
 use Entity\User;
+use Entity\Mail;
 use Ui\Message;
 
 /**
@@ -36,10 +37,30 @@ class Register extends AbstractController {
             $user->setMail($this->getParam('mail'));
             $user->setPassword($this->getParam('password'));
             $errors = $user->getDao($this->getEm())->persist($user, $this->getTranslator(), true);
-            
             if (sizeof($errors) === 0) {
-                // Show confirmation
-                $this->renderTemplate('register_success');
+                $mail = new Mail();
+                // TODO:
+                // in productiv-system: Send EMail and set this var to true. 
+                $mail->setIsSent(false);
+                $sMail = $mail->setReceiverMail($user->getMail());
+                $mail->setSentDate($user->getRegDate());
+                $sSubject = $mail->setSubject(gettext('mail.register.subject'));
+                $sContent = $mail->setContent(gettext('mail.register.content'));
+                $errors = $mail->getDao($this->getEm())->persist($mail, $this->getTranslator(), true);
+                if (sizeof($errors) === 0) {
+                    // Send mail
+                    // TODO: eine Tabelle in der Datenbank anlegen, in der Configurationne hinterlegt sind
+                    // So zum Beispiel auch die Absender-EMailAdresse des Systems (vielleicht die E-Mail des Sideadmins?)
+                    // $sFrom = "s3002549@rz.ba-dresden.de";
+                    // mail($sMail, $sSubject, $sContent, $sFrom);
+                    
+                    // Show confirmation                    
+                    $this->renderTemplate('register_success');
+                } else {
+                    // Render registration form again.
+                    $this->addMessages($errors);
+                    $this->renderTemplate('register');
+                }
             } else {
                 // Render registration form again.
                 $this->addMessages($errors);
