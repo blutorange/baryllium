a4b0a2d8f2c83b7d6e3bba8b0836a70ac070d008
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2017-03-17 00:53:24 +0100

    [intern] CampusDual mostly done, see sandbox.php for a demo

--- private/sandbox.php
+++ private/sandbox.php
@@ -1,15 +1,16 @@
 <?php
 
+use Extension\CampusDual\CampusDualLoader;
+
 
 /* Use this for quickly testing some php code... */
 
 require_once './bootstrap.php';
 
-//$matches = [];
-//$pat = '/hash\s*=\s*["\']([0-9a-f]{16,64})[\'"]/i';
-//$stuff = 'qwe qwe hash="80b9f7eee12f785849dd092e81c7d3fF";user="3002591"; qwe asd';
-//var_dump(preg_match($pat, $stuff, $matches));
-//var_dump($matches);
-
-$l = new Extension\CampusDual\CampusDualLoader("3002591", "11035cre&$!");
-$l->getCourse(); 
+CampusDualLoader::perform("3002591", "secretPassword", function(CampusDualLoader $loader) {
+//    $start = time();
+//    $end = time()+7*24*60*60;
+//    var_dump($loader->getTimeTableRaw($start, $end));
+//    var_dump($loader->getMetaRaw());
+    var_dump($loader->getStudyGroup());
+});
