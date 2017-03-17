0426fa98e26a3fcf15cfd3e96001dba7ddcd49fe
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2017-03-17 22:25:20 +0100

    [intern] CampusDualLogin png, refactored

--- private/sandbox.php
+++ private/sandbox.php
@@ -7,10 +7,31 @@ use Extension\CampusDual\CampusDualLoader;
 
 require_once './bootstrap.php';
 
-CampusDualLoader::perform("3002591", "secretPassword", function(CampusDualLoader $loader) {
-//    $start = time();
-//    $end = time()+7*24*60*60;
-//    var_dump($loader->getTimeTableRaw($start, $end));
-//    var_dump($loader->getMetaRaw());
-    var_dump($loader->getStudyGroup());
-});
+class A {
+    private $x = 5;
+    public function call($m, $val) {
+        $m($val);
+    }
+    public function set($val) {
+        
+        return $this->call($this::setX, $val);
+    }
+    public function getX() {
+        return $x;
+    }
+    private static function setX(A $a, $val) {
+        $a->x = $val;
+    }
+}
+
+$a = new A();
+$a->set(5);
+echo $a->getX();
+
+//CampusDualLoader::perform("3002591", "secretPassword", function(CampusDualLoader $loader) {
+////    $start = time();
+////    $end = time()+7*24*60*60;
+////    var_dump($loader->getTimeTableRaw($start, $end));
+////    var_dump($loader->getMetaRaw());
+//    var_dump($loader->getStudyGroup());
+//});
