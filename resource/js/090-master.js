c2cfc480858cbca5cd5466fba84a00908cae58e8
CaptainMalzbier <c4ptain.malzbier@gmail.com>
2017-03-28 12:30:49 +0200

    [fix] removed title input field when creating a new post [feature] 	smooth animation when loading a page with 			setting opacity to 0 in 090.master.less

--- resource/js/090-master.js
+++ resource/js/090-master.js
@@ -4,21 +4,20 @@
  * and open the template in the editor.
  */
 $('document').ready(function () {
-    $('body').css('opacity', 0);
-    $('body').animate({opacity: '1'}, "slow");
-    
+    $("body").animate({'opacity':'1'},350);
+
     // Setup parsley for forms.
     window.parsley.setLocale(window.moose.locale);
     $('[data-bootstrap-parsley]').parsley({
         successClass: 'has-success',
         errorClass: 'has-error',
-        classHandler: function(field) {
+        classHandler: function (field) {
             return field.$element.closest('.form-group');
         },
         errorsWrapper: '<ul class=\"help-block\"></ul>',
         errorElem: '<li></li>'
     });
-    
+
     // Enable infinite scroll
     var img = document.createElement('img');
     img.alt = 'Loading';
@@ -30,7 +29,7 @@ $('document').ready(function () {
         contentSelector: '.jscroll-content',
         pagingSelector: '.jscroll-paging',
         loadingDelay: 1000,
-        callback: function(){
+        callback: function () {
             var me = $(this);
             var destroy = me.find(".jscroll-destroy");
             if (destroy.length > 0) {
@@ -41,14 +40,15 @@ $('document').ready(function () {
 
     // Enable inline editing of posts.
     var markdownEditing = false;
-    $('[data-provide="markdown-loc-editable"]').on("click", function(){
-        if ($.LoadingOverlay("active") || markdownEditing) return;
+    $('[data-provide="markdown-loc-editable"]').on("click", function () {
+        if ($.LoadingOverlay("active") || markdownEditing)
+            return;
         var me = $(this);
         var oldContent = null;
         var updateUrl = me.data('updateurl');
         var old = me.clone(true, false).empty();
         var blurs = 0;
-        var onSave = function(editor) {
+        var onSave = function (editor) {
             var content = editor.parseContent();
             if (oldContent === null || oldContent == editor.getContent()) {
                 old.append(content);
@@ -65,21 +65,20 @@ $('document').ready(function () {
                 data: {
                     content: content
                 },
-            }).done(function(data, textStatus, jqXHR) {
+            }).done(function (data, textStatus, jqXHR) {
                 var error = data.error;
                 if (error) {
-                    var message = (error||{}).message || 'Unhandled error';
-                    var details = (error||{}).details || 'Failed to save post, please try again later.';                    
+                    var message = (error || {}).message || 'Unhandled error';
+                    var details = (error || {}).details || 'Failed to save post, please try again later.';
                     alert(message + ": " + details);
-                }
-                else {
+                } else {
                     old.append(content);
                     editor.$editor.replaceWith(old);
                     markdownEditing = false;
                 }
-            }).fail(function(jqXHR, textStatus, errorThrown) {
+            }).fail(function (jqXHR, textStatus, errorThrown) {
                 alert("Could not save post (" + textStatus + "): " + errorThrown);
-            }).always(function(dataOrJqXHR, textStatus, jqXHROrErrorThrown) {
+            }).always(function (dataOrJqXHR, textStatus, jqXHROrErrorThrown) {
                 $.LoadingOverlay('hide');
             });
         }
@@ -87,12 +86,12 @@ $('document').ready(function () {
         $(this).markdown({
             savable: true,
             onSave: onSave,
-            onShow: function(editor) {
+            onShow: function (editor) {
                 if (oldContent === null) {
                     oldContent = editor.getContent();
                 }
             },
-            onBlur: function(editor) {
+            onBlur: function (editor) {
                 blurs++;
                 if (blurs > 1) {
                     onSave(editor);
@@ -102,12 +101,12 @@ $('document').ready(function () {
     });
 
     // Setup markdown editor (for posts etc.)
-    $('[data-provide="markdown-loc"]').each(function(){
+    $('[data-provide="markdown-loc"]').each(function () {
         //console.log(e.parseContent());    
         var input = $(document.getElementById(this.id + "-hidden"));
         $(this).markdown({
             language: window.moose.locale,
-            onBlur: function(e) {
+            onBlur: function (e) {
                 input.val(e.parseContent());
             },
             dropZoneOptions: {
@@ -116,32 +115,33 @@ $('document').ready(function () {
                 maxFilesize: 2, // MB
                 thumbnailHeight: 32,
                 previewTemplate: '<div class="dropzone"><div class="dz-preview dz-file-preview"><div class="dz-details"><div class="dz-filename"><span data-dz-name></span></div><div class="dz-size" data-dz-size></div><img data-dz-thumbnail/></div>  <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>  <div class="dz-success-mark"><span>✔</span></div>  <div class="dz-error-mark"><span>✘</span></div><div class="dz-error-message"><span data-dz-errormessage></span></div></div></div>',
-                accept: function(file, done) {
-                  if (file.name == "justinbieber.jpg") {
-                    done("Naha, you don't.");
-                  }
-                  else { done(); }
+                accept: function (file, done) {
+                    if (file.name == "justinbieber.jpg") {
+                        done("Naha, you don't.");
+                    } else {
+                        done();
+                    }
                 }
             },
             //hiddenButtons: ['cmdImage'],
             additionalButtons: [
                 [{
-                      name: "groupLink",
-                      data: [{
-                        name: "cmdCustomImage",
-                        toggle: false,
-                        title: "Insert image",
-                        icon: {
-                            glyph: 'glyphicon glyphicon-upload',
-                            fa: 'fa fa-picture-o',
-                            'fa-3': 'icon-picture',
-                            octicons: 'octicon octicon-file-media'
-                        },
-                        callback: function(editor){
-                            editor.$editor.trigger('click');
-                        }
+                        name: "groupLink",
+                        data: [{
+                                name: "cmdCustomImage",
+                                toggle: false,
+                                title: "Insert image",
+                                icon: {
+                                    glyph: 'glyphicon glyphicon-upload',
+                                    fa: 'fa fa-picture-o',
+                                    'fa-3': 'icon-picture',
+                                    octicons: 'octicon octicon-file-media'
+                                },
+                                callback: function (editor) {
+                                    editor.$editor.trigger('click');
+                                }
+                            }]
                     }]
-                }]
             ]
         });
     });
