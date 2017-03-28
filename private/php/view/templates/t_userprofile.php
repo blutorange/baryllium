d60ad21f974ec38417ab8a1f2a3ecb40c69593c3
CaptainMalzbier <c4ptain.malzbier@gmail.com>
2017-03-28 18:02:29 +0200

    [feature] added user setting to enable/disable infinite scrolling

--- private/php/view/templates/t_userprofile.php
+++ private/php/view/templates/t_userprofile.php
@@ -1,23 +1,48 @@
 <?php $this->layout('portal', ['title' => 'User profile']); ?>
 
-<?php     
+<?php
 //   $tmp = new \Entity\User();
-    $avatar = $user->getAvatar();
-    $tutorialGroup = $user->getTutorialGroup();
-    $tutorialGroupName = $tutorialGroup !== null ? $tutorialGroup->getCompleteName() : null;
-    $fieldOfStudy = $tutorialGroup !== null ? $tutorialGroup->getFieldOfStudy() : null;
-    $discipline = $fieldOfStudy !== null ? $fieldOfStudy->getDiscipline() : null;
-    $subdiscipline = $fieldOfStudy !== null ? $fieldOfStudy->getSubDiscipline() : null;
+$avatar = $user->getAvatar();
+$tutorialGroup = $user->getTutorialGroup();
+$tutorialGroupName = $tutorialGroup !== null ? $tutorialGroup->getCompleteName()
+            : null;
+$fieldOfStudy = $tutorialGroup !== null ? $tutorialGroup->getFieldOfStudy() : null;
+$discipline = $fieldOfStudy !== null ? $fieldOfStudy->getDiscipline() : null;
+$subdiscipline = $fieldOfStudy !== null ? $fieldOfStudy->getSubDiscipline() : null;
 ?>
 
 <p>
-    <span><?= $this->e($user->getFirstName())?></span>, <span><?= $this->e($user->getLastName())?></span>
+    <span><?= $this->e($user->getFirstName()) ?></span>, <span><?= $this->e($user->getLastName()) ?></span>
 </p>
-<p> <?= $this->e($user->getStudentId())?> </p>
-<p> <?= $this->e($discipline ?? "Unspecified")?> / <?= $this->e($subdiscipline ?? "Unspecified")?></p>
-<p> <?= $this->e($tutorialGroupName ?? "Unspecified")?> </p>
+<p> <?= $this->e($user->getStudentId()) ?> </p>
+<p> <?= $this->e($discipline ?? "Unspecified") ?> / <?=
+    $this->e($subdiscipline ?? "Unspecified")
+    ?></p>
+<p> <?= $this->e($tutorialGroupName ?? "Unspecified") ?> </p>
+    <form>
+        <label for="infinite-scrolling"><?= $this->egettext('infinitescrolling.enable') ?></label>
+        <input type="checkbox" id="infinite-scrolling" name="infinite-scrolling">
+    </form>
 <?php if ($avatar !== null): ?>
-    <img class="avatar" src="<?= $this->e($user->getAvatar())?>"/>
+    <img class="avatar" src="<?= $this->e($user->getAvatar()) ?>"/>
 <?php else: ?>
     <p>No avatar set</p>
-<?php endif;
+<?php endif; ?>
+<script type="text/javascript">
+    $(document).ready(function () {
+        if (window.moose.getFromLocalStorage() === "1") {
+            $("input[name=infinite-scrolling]").prop('checked', true);
+        } else {
+            $("input[name=infinite-scrolling]").prop('checked', false);
+        }
+
+        $("input[name=infinite-scrolling]").click(function () {
+            if ($("input[name=infinite-scrolling]").prop('checked')) {
+                localStorage['infinite-scrolling'] = 0;
+            } else {
+                localStorage['infinite-scrolling'] = 1;
+            }
+        });
+    });
+</script>
+<?php
