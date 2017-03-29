6ebfde1b3e6763c22a68417e6dcfa74fe8601866
Trisha <Trisha@Mike>
2017-03-29 15:25:14 +0200

    [styling] styling user profile, changing main font

--- private/php/view/templates/t_userprofile.php
+++ private/php/view/templates/t_userprofile.php
@@ -11,21 +11,25 @@
 ?>
 
 <section>
-    <div id="profile_avatar">
-        <?php if ($avatar !== null): ?>
-        <img class="avatar" id="profile_avatar_img" src="<?= $this->e($user->getAvatar())?>"/>
-        <?php else: ?>
-            <p>No avatar set</p>
-        <?php endif; ?>
-    </div>
-   
-    <div class="moose_border moose_grey" id="profile_distance_img">
-        <p id="profile_name">
-            <span><?= $this->e($user->getFirstName())?></span>, <span><?= $this->e($user->getLastName())?></span>
-        </p>
-        <p class="info_user"> <?= $this->e($user->getStudentId())?> </p>
-        <p class="info_user"> <?= $this->e($discipline ?? "Unspecified")?> / <?= $this->e($subdiscipline ?? "Unspecified")?></p>
-        <p class="info_user"> <?= $this->e($tutorialGroupName ?? "Unspecified")?> </p>
+    <div class="moose-white">
+        <div class="profile-avatar-area">
+            <div id="profile_avatar">
+            <?php if ($avatar !== null): ?>
+            <img class="avatar" id="profile_avatar_img" src="<?= $this->e($user->getAvatar())?>"/>
+            <?php else: ?>
+                <p>No avatar set</p>
+            <?php endif; ?>
+            </div> 
+        </div>
+        
+        <div>  
+            <p id="profile_name">
+                <span><?= $this->e($user->getFirstName())?></span> <span><?= $this->e($user->getLastName())?></span>
+            </p>
+            <p class="profile-info-user"> <?= $this->e($user->getStudentId())?> </p>
+            <p class="profile-info-user"> <?= $this->e($discipline ?? "Unspecified")?> / <?= $this->e($subdiscipline ?? "Unspecified")?></p>
+            <p class="profile-info-user"> <?= $this->e($tutorialGroupName ?? "Unspecified")?> </p>
+        </div>  
     </div>
 
 
