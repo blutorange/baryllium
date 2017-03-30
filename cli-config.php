8786f953694485ad8ed407835d4b58e324b54ee0
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2017-03-30 20:44:37 +0200

    [fix] The context is a singleton now, changed remaining reference.

--- cli-config.php
+++ cli-config.php
@@ -4,4 +4,4 @@ use Doctrine\ORM\Tools\Console\ConsoleRunner;
 // replace with file to your own project bootstrap
 require_once './private/bootstrap.php';
 
-return ConsoleRunner::createHelperSet($context->getEm());
+return ConsoleRunner::createHelperSet(Context::getInstance()->getEm());
