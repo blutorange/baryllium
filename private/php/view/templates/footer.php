d7acc071b90dc8c79f19713f1c2d9279966fc145
Trisha <Trisha@Mike>
2017-04-24 23:04:34 +0200

    [styling] Modell für footer (unsere vision, kontaktaufnahme, agb, impressum, datenschutz), footer in neuer Datei footer.php

---
+++ private/php/view/templates/footer.php
@@ -0,0 +1,42 @@
+<div id="footer">
+    <div id="footer_left">
+        
+            <!-- Auslesen der Textdatei, jedoch wird die Textdatei nicht gefunden -->
+            <?php
+//            $fh = fopen( "barrylium/unsere-vision.txt", 'r' );
+//        
+//            if  ($fh == ""){
+//                echo "Fehler";
+//            }
+//            else{
+//                echo "Es geht";
+//                    while ( !feof( $fh ) ) 
+//                    {
+//                       $line = trim( fgets( $fh ) );
+//
+//                       // do something with $line
+//                    }
+//            }
+// 
+//            fclose( $fh ); 
+//            ?>
+            <div class="bold"> Unsere Vision </div>
+            <div> Wir wollen den hartarbeitenden dualen Studenten entlasten, indem wir eine Plattform für alle  Informationen bieten </div>
+       
+        
+    </div>
+    <div id="footer_center">
+        <p class="text-muted">Du suchst den Kontakt zu uns?</p>
+        <button class="button" type="button">Schreib uns!</button>
+    </div>
+    <div id="footer_right">
+        <p class="text-muted">
+            <ul>
+                <li>Impressum</li>
+                <li>Datenschutz</li>
+                <li>AGB</li>
+            </ul>
+        </p>    
+    </div>
+</div>
+
